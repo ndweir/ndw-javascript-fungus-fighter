6224901219d8138fb3fd2ce3af63e9058a73694d
nweir <ndweir13@gmail.com>
2024-09-09 17:29:05 -0500

    firstAttempt

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 // State Variables can be declared outside of the onReady
 // Feel free to make this to what you want!
 // Example:
-// let fungusHP = 100;
+ let fungusHP = 100;
+ let yourAP = 100;
+
+let arcaneAP = 12;
+let arcaneHP = 14;
+
+let entangleAP = 23;
+let entangleHP = 9;
+
+let dragonAP = 38;
+let dragonHP = 47;
+
+let starAP = 33;
+let starHP = 25;
 
 function onReady() {
     console.log("Ready to go!")
@@ -14,7 +27,33 @@ function onReady() {
     // - Handle events that ->
     // - Updates state which is ->
     // - Rendered to the DOM
+    render()
+}
+
+onReady()
+
+function render(){
+    let currentAP = document.getElementById('currentAPtext').innerText
+    let enemyHP = document.getElementById("currentEnemyHPtext").innerText
+
+
 }
 
 
-onReady()
+
+function updateAPandHP(HPvalue, APvalue){
+    let apMeter = document.getElementById('ap-meter').value
+
+    apMeter -= APvalue;
+
+    let hpMeter = document.getElementById("hp-meter").value
+
+    hpMeter -= HPvalue;
+
+    console.log(apMeter, hpMeter);
+
+
+    fungusHP = hpMeter;
+    yourAP = apMeter;
+    render()
+}
